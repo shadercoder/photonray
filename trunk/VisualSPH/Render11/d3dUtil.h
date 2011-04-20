7a2cfbd0dc8383864e74967d7f5fbddb6dfbcc46
ilyakrukov@gmail.com <ilyakrukov@gmail.com@f2d442f5-4fb6-3e1e-f2a6-32ff9b5dcf3a>
2011-04-20 16:51:50 +0000

    update

--- trunk/VisualSPH/Render11/d3dUtil.h
+++ trunk/VisualSPH/Render11/d3dUtil.h
@@ -27,6 +27,47 @@
 #include <dxerr.h>
 #include <cassert>
 
+//--------------------------------------------------------------------------------------
+// Helper for creating structured buffers with an SRV and UAV
+//--------------------------------------------------------------------------------------
+template <class T>
+HRESULT CreateStructuredBuffer(ID3D11Device* pd3dDevice, UINT iNumElements, ID3D11Buffer** ppBuffer, ID3D11ShaderResourceView** ppSRV, ID3D11UnorderedAccessView** ppUAV, const T* pInitialData = NULL)
+{
+    HRESULT hr = S_OK;
+
+    // Create SB
+    D3D11_BUFFER_DESC bufferDesc;
+    ZeroMemory( &bufferDesc, sizeof(bufferDesc) );
+    bufferDesc.ByteWidth = iNumElements * sizeof(T);
+    bufferDesc.Usage = D3D11_USAGE_DEFAULT;
+    bufferDesc.BindFlags = D3D11_BIND_UNORDERED_ACCESS | D3D11_BIND_SHADER_RESOURCE;
+    bufferDesc.MiscFlags = D3D11_RESOURCE_MISC_BUFFER_STRUCTURED;
+    bufferDesc.StructureByteStride = sizeof(T);
+
+    D3D11_SUBRESOURCE_DATA bufferInitData;
+    ZeroMemory( &bufferInitData, sizeof(bufferInitData) );
+    bufferInitData.pSysMem = pInitialData;
+    V_RETURN( pd3dDevice->CreateBuffer( &bufferDesc, (pInitialData)? &bufferInitData : NULL, ppBuffer ) );
+
+    // Create SRV
+    D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc;
+    ZeroMemory( &srvDesc, sizeof(srvDesc) );
+    srvDesc.Format = DXGI_FORMAT_UNKNOWN;
+    srvDesc.ViewDimension = D3D11_SRV_DIMENSION_BUFFER;
+    srvDesc.Buffer.ElementWidth = iNumElements;
+    V_RETURN( pd3dDevice->CreateShaderResourceView( *ppBuffer, &srvDesc, ppSRV ) );
+
+    // Create UAV
+    D3D11_UNORDERED_ACCESS_VIEW_DESC uavDesc;
+    ZeroMemory( &uavDesc, sizeof(uavDesc) );
+    uavDesc.Format = DXGI_FORMAT_UNKNOWN;
+    uavDesc.ViewDimension = D3D11_UAV_DIMENSION_BUFFER;
+    uavDesc.Buffer.NumElements = iNumElements;
+    V_RETURN( pd3dDevice->CreateUnorderedAccessView( *ppBuffer, &uavDesc, ppUAV ) );
+
+    return hr;
+}
+
 
 //*****************************************************************************
 // Simple d3d error checker 
